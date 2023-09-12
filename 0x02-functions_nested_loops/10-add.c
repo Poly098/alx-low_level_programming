af2c2bdf4a507044866c2146e926e053f1c44a5e
Polycarp <ebepolycarp098@gmail.com>
2023-09-12 14:47:04 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two numbers
+ * @e: first parameter
+ * @f: second parameter
+ * Return: e + f
+ */
+
+int add(int e, int f)
+{
+	return (e + f);
+}
