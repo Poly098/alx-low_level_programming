c53cdefd0e9a6e1a746e660a963f3f87fe6d1415
Polycarp <ebepolycarp098@gmail.com>
2023-09-14 18:33:06 -0700

    multiply

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _isdigit - function that check for 0 through 9
+ * _isdigit - function that checks 0 through 9
  * @c: functional parameter
  * Return: 1 or 0
  */
 
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
+	if (c >= 0 && c <= 9)
 		return (1);
-	else
+	else if (c >= 'a' && c < = 'z')
 		return (0);
 }
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - funtion that multiples two integers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
