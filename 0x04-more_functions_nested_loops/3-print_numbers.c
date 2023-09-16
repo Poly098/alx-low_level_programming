8b03258fb82f48bffca3cd1dba980e1ecffbc91a
Polycarp <ebepolycarp098@gmail.com>
2023-09-15 19:03:42 -0700

    print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - print numbers from 0 through 9
- * @i: main parameter
  * Return: void
  */
 
 void print_numbers(void)
 {
 	int i;
-	char _putchar();
+
+	char _putchar(void);
 
 	for (i = 0 ; i <= 9 ; i++)
 		_putchar(i + '0');
